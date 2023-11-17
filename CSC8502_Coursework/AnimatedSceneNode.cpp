4e5c524fe695cc75d34b3bf4f13151352c0382db
eren.degirmenci <eren.degirmenci@inventuna.com>
2023-11-17 01:13:26 +0000

    feat(logic): added animated scene node class

---
+++ CSC8502_Coursework/AnimatedSceneNode.cpp
@@ -0,0 +1,68 @@
+#include "AnimatedSceneNode.h"
+#include "../nclgl/MeshAnimation.h"
+#include "../nclgl/MeshMaterial.h"
+
+AnimatedSceneNode::AnimatedSceneNode(Mesh* animatedMesh, MeshAnimation* animatedMeshAnimation, MeshMaterial* animatedMeshMaterial) : SceneNode(animatedMesh){
+	_meshAnimation = animatedMeshAnimation;
+	_meshMaterial = animatedMeshMaterial;
+	for (int i = 0; i < _mesh->GetSubMeshCount(); ++i) {
+		const MeshMaterialEntry* matEntry = _meshMaterial->GetMaterialForLayer(i);
+
+		const string* fileName = nullptr;
+		matEntry->GetEntry("Diffuse", &fileName);
+		string path = TEXTUREDIR + *fileName;
+		GLuint texID = SOIL_load_OGL_texture(path.c_str(), SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y);
+		_materialTextures.emplace_back(texID);
+	}
+
+	_currentFrame = 0;
+	_currentFrameTime = 0.f;
+}
+
+void AnimatedSceneNode::setUpShader(OGLRenderer& renderer){
+
+	renderer.BindShader(_shader);
+	Matrix4 modelMatrix = getWorldTransform() * Matrix4::Scale(getModelScale());
+	renderer.setModelMatrix(modelMatrix);
+	renderer.UpdateShaderMatrices();
+
+	glUniform1i(glGetUniformLocation(_shader->GetProgram(), "diffuseTex"), 0);
+	vector<Matrix4> frameMatrices;
+	const Matrix4* invBindPose = _mesh->GetInverseBindPose();
+	const Matrix4* frameData = _meshAnimation->GetJointData(_currentFrame);
+
+	for (unsigned int i = 0; i < _mesh->GetJointCount(); i++) {
+		frameMatrices.emplace_back(frameData[i] * invBindPose[i]);
+	}
+
+	int j = glGetUniformLocation(_shader->GetProgram(), "joints");
+	glUniformMatrix4fv(j, frameMatrices.size(), false, (float*)frameMatrices.data());
+
+}
+
+void AnimatedSceneNode::draw(OGLRenderer& renderer, bool isDrawingForShadow){
+	glEnable(GL_CULL_FACE);
+	if (isDrawingForShadow){
+		return;
+	}
+
+	if (_shader){
+		setUpShader(renderer);
+	}
+
+	for (int i = 0; i < _mesh->GetSubMeshCount(); i++) {
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, _materialTextures[i]);
+		_mesh->DrawSubMesh(i);
+	}
+	glDisable(GL_CULL_FACE);
+}
+
+void AnimatedSceneNode::update(float dt){
+	SceneNode::update(dt);
+	_currentFrameTime -= dt;
+	while (_currentFrameTime < .0f) {
+		_currentFrame = (_currentFrame + 1) % _meshAnimation->GetFrameCount();
+		_currentFrameTime += 1.f / _meshAnimation->GetFrameRate();
+	}
+}
---
+++ CSC8502_Coursework/AnimatedSceneNode.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "SceneNode.h"
+
+class MeshAnimation;
+class MeshMaterial;
+
+class AnimatedSceneNode : public SceneNode {
+public:
+	AnimatedSceneNode(Mesh* animatedMesh, MeshAnimation* animatedMeshAnimation, MeshMaterial* animatedMeshMaterial);
+protected:
+
+	int _currentFrame = 0;
+	float _currentFrameTime = 0.f;
+	MeshAnimation* _meshAnimation;
+	MeshMaterial* _meshMaterial;
+
+	vector<GLuint> _materialTextures;
+
+	void setUpShader(OGLRenderer& renderer) override;
+	void draw(OGLRenderer& renderer, bool isDrawingForShadow);
+	void update(float dt) override;
+};
